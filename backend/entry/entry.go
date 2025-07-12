15e44414cb29c5c6a3aaf922d4a742b21a34bf26
kyanhvo <tommyvo0406@gmail.com>
2025-07-12 18:55:29 -0500

    PredictRealFakeHandler now turns body into JSON and ensures text and title keys

--- backend/entry/entry.go
+++ backend/entry/entry.go
@@ -1,17 +1,67 @@
-package main;
+package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 )
 
-func PredictRealFakeHandler(write http.ResponseWriter, read *http.Request) {
-	// TODO: handle json, tcp to fake_news_identifier.py,
-	// get response, and send back to user.
+func HandleUnexpectedError(err error) {
+	if (err == nil){
+		return
+	}
+	fmt.Printf("%v\n", err)
+	os.Exit(1)
+}
+
+func PredictRealFakeHandler(w http.ResponseWriter, r *http.Request) {
+	// Ensure method is POST
+	
+	if (r.Method != "POST") {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("{\"error\": \"predict_real_fake_news requires POST method}"))
+		return
+	}
+
+	// Get bodyBytes
+
+	bodyBytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		msg := map[string]string { "error": "failure to read request body" }
+		msgBytes, _ := json.Marshal(msg)
+		w.Write(msgBytes)
+		return
+	}
+
+	// Get bodyMap from body, ensure json, ensure also 'title' and 'text' are
+	// keys of such json before sending it over to model
+
+	var bodyMap map[string]string
+	err = json.Unmarshal(bodyBytes, &bodyMap)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("{\"error\": \"cannot parse request's body in JSON\"}"))
+		return
+	}
+
+	if _, containsTitle := bodyMap["title"]; !containsTitle {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("{\"error\": \"predict_real_fake_news requires title key}"))
+		return
+	}
+	if _, containsText := bodyMap["text"]; !containsText {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("{\"error\": \"predict_real_fake_news requires text key}"))
+		return
+	}
+	
+	// TODO: Send this to model via loopback:5000
 }
 
 func main() {
-	http.HandleFunc("/predict_real_fake", PredictRealFakeHandler)
-	fmt.Println("Server listening on port 5000")
-	http.ListenAndServe(":5000", nil)
+	http.HandleFunc("/api/predict_real_fake_news", PredictRealFakeHandler)
+	http.ListenAndServe(":80", nil)
 }
